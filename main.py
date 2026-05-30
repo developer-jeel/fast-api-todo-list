ede1dfdf0622a4212255b541c5c95e77919428ed
jeel tank <jeeltank.developer@gmail.com>
2026-05-30 19:19:40 +0530

    todo update

--- main.py
+++ main.py
@@ -23,5 +23,6 @@ def create_todos(todo : ToDo):
 def update_todos(todo_id:int , updated_todo : ToDo):
     for i , todo in enumerate(todos):
         if todo.id == todo_id:
-            
-    return {"message" : "todo updated sucessfully"}
+            todo[i] = updated_todo
+            return {"message" : "todo updated sucessfully"}
+    return {"message" : "feild todo"}
