deb0f245a459e0305a83cacce4a8b012555d350e
jeel tank <jeeltank.developer@gmail.com>
2026-05-30 19:17:24 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -22,5 +22,6 @@ def create_todos(todo : ToDo):
 @app.put('/{todo_id}')
 def update_todos(todo_id:int , updated_todo : ToDo):
     for i , todo in enumerate(todos):
-        
+        if todo.id == todo_id:
+            
     return {"message" : "todo updated sucessfully"}
