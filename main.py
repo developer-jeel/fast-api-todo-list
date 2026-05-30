b92dd8dbd1c54f795ab85be3aebc28e8d5149080
jeel tank <jeeltank.developer@gmail.com>
2026-05-30 19:16:33 +0530

    modified:   __pycache__/main.cpython-313.pyc 	modified:   main.py

--- main.py
+++ main.py
@@ -21,4 +21,6 @@ def create_todos(todo : ToDo):
 
 @app.put('/{todo_id}')
 def update_todos(todo_id:int , updated_todo : ToDo):
+    for i , todo in enumerate(todos):
+        
     return {"message" : "todo updated sucessfully"}
