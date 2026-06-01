b2115d41852396b93d482953fe1afc4ae9112319
jeel tank <jeeltank.developer@gmail.com>
2026-06-01 21:05:58 +0530

    ..

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 from database import engine
 from models import Base
 from crud import router as crud_router
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(crud_router,prefix='/todo',tags=["Crud Router"])
+
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1" ,port="8000")
