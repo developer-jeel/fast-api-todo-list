44b021781ffeac430b75ae67a47ec9212b389d0e
jeel tank <jeeltank.developer@gmail.com>
2026-06-01 14:27:42 +0530

    new file:   crud.py 	modified:   main.py

---
+++ crud.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class ToDo(BaseModel):
+    id : int
+    name : str
+    description : str
+
+todos = []
+
+@app.get('/')
+def show_todos():
+    return todos
+
+@app.post('/')
+def create_todos(todo : ToDo):
+    todos.append(todo)
+    return {"message" : "todo created sucessfully"}
+
+@app.put('/{todo_id}')
+def update_todos(todo_id:int , updated_todo : ToDo):
+    for i , todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos[i] = updated_todo
+            return {"message" : "todo updated sucessfully"}
+    return {"message" : "feild todo"}
+
+@app.delete('/{todo_id}')
+def delete_todos(todo_id:int):
+    global todos
+    todos = [todo for todo in todos if todo.id != todo_id ]
+    return {"message" : " todo deleted"}
