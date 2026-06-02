472e28bd9013ee194659a82e256401bc3ef894b6
jeel tank <jeeltank.developer@gmail.com>
2026-06-02 14:36:25 +0530

    .

--- crud.py
+++ crud.py
@@ -27,6 +27,14 @@ def show_todos(request : Request ,db : Session = Depends(get_db)):
     todos =  db.query(ToDo).all()
     return templates.TemplateResponse(request, name='todo_list.html', context={"todos":todos})
 
+@router.get('/',response_model=ToDoResponse)
+def create_todos(todo : ToDoCreate,db : Session = Depends(get_db)):
+    new_todo = ToDo(title = todo.title,description = todo.description,done = todo.done)
+    db.add(new_todo)
+    db.commit()
+    db.refresh(new_todo)
+    return new_todo
+
 @router.post('/',response_model=ToDoResponse)
 def create_todos(todo : ToDoCreate,db : Session = Depends(get_db)):
     new_todo = ToDo(title = todo.title,description = todo.description,done = todo.done)
@@ -47,11 +55,11 @@ def update_todos(todo_id:int,todo : ToDoCreate ,db : Session = Depends(get_db)):
     db.commit()
     return db_todo
 
-@router.delete('/{todo_id}')
+@router.get('/delete/{todo_id}')
 def delete_todos(request: Request,todo_id:int,db : Session = Depends(get_db)):
     db_todo = db.query(ToDo).filter(ToDo.id == todo_id).first()
     if not db_todo:
         return HTTPException(status_code=404,detail="Todo not found")
     db.delete(db_todo)
     db.commit()
-    return templates.TemplateResponse(request, name='todo_list.html', context={"todos":todos})
+    return RedirectResponse(url="/todo/", status_code=303)
